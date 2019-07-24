754a7d640f57a8194ef6cea3414ee0764555ef98
Mario Johnson <johnson.mario8404@gmail.com>
2019-07-24 14:11:18 -0500

    Completed Java intro Exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,12 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello, World!");
         System.out.print("Hello, World!");
+
     }
 }
+
+
+//Type "psvm" and then tab to quickly print out "public static void main(String[] args) {
+//
+//    }"
+//Type "sout" and than tab to quickly print out "System.out.println();"

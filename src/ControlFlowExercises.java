0b49f1a9f0822a0910cee2201379bcc6d4f63871
Mario Johnson <johnson.mario8404@gmail.com>
2019-07-26 12:30:45 -0500

    Updated Control Statements and Loops Exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,6 +1,4 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
-
 public class ControlFlowExercises {
         static void fizzBuzz(int num) {
             for (int i = 1; i <= num; i++) {
@@ -16,41 +14,67 @@ public class ControlFlowExercises {
             }
         }
     public static void main(String[] args) {
+
 //While
         int i = 5;
         while (i <= 15) {
-            System.out.println(i++);
+            System.out.print(i);
+            System.out.print(" ");
+            i++;
         }
 
 //Do While
-        i = 0;
+        int j = 0;
         do {
-            System.out.println(i);
-            i += 2;
-        } while (i <= 100);
+            System.out.println(j);
+            j += 2;
+        } while (j <= 100);
 
 
-        i = 100;
+        int k = 100;
         do {
-            System.out.println(i);
-            i -= 5;
-        } while (i >= -10);
+            System.out.println(k);
+            k -= 5;
+        } while (k >= -10);
 
-       long n = 2;
+
+       long l = 2;
         do {
-            System.out.println(n);
-            n *= n;
-        } while(n < 1000000);
+            System.out.println(l);
+            l *= l;
+        } while(l < 1000000);
 
+//        refactored while loop
+        for (int m = 5; m <= 15; m++){
+            System.out.print(m);
+            System.out.print(" ");
+        }
 // We have to call the fizzbuzz method inside the main method. Since it sits outside of it.
         fizzBuzz(100);
+        squaresCubes();
+        }
+        private static void squaresCubes(){
+            Scanner scan = new Scanner(System.in);
+            String answer;
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a whole number: ");
-        int userInput = scanner.nextInt();
-        System.out.println("you entered: " + userInput);
+            do {
+                System.out.println("What number would you like to go up to? ");
+                int userNum = scan.nextInt();
+                System.out.println();
 
+                System.out.println("Here is your table!");
+                System.out.println();
 
+                System.out.println("number | squared | cubed");
+                System.out.println("------ | ------- | -----");
+                for (long i = 1; i <= userNum; i++) {
+                    System.out.printf("%-7d|%-9d|%-8d%n", i, i * i, i * i * i);
+                }
+                System.out.println();
+                System.out.println("Do you want to continue (y/n)? ");
+                answer = scan.next();
+            } while (answer.equalsIgnoreCase("y"));
 
+            System.out.println("Thank you for playing!");
     }
 }

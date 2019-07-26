5118f675a40a890fedfb204f40102161bbbcfc41
Mario Johnson <johnson.mario8404@gmail.com>
2019-07-26 17:00:32 -0500

    Updated Strings Exercise

---
+++ src/Bob.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Whats your question for Bob?: ");
+//        String bobsAnswer = scan.next();
+    }
+}

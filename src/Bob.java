da0db2b52f1bb6e0488044a777843fca849dd945
Mario Johnson <johnson.mario8404@gmail.com>
2019-07-29 09:44:40 -0500

    Completed Strings Exercise

--- src/Bob.java
+++ src/Bob.java
@@ -3,7 +3,23 @@ import java.util.Scanner;
 public class Bob {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Whats your question for Bob?: ");
-//        String bobsAnswer = scan.next();
+        System.out.print("Bob is being lazy per usual...");
+
+        do {
+            String message = scan.nextLine();
+
+            if (message.endsWith("?")) {
+                System.out.println("Sure.");
+            } else if (message.endsWith("!")) {
+                System.out.println("Whoa chill out!");
+            } else if (message.isEmpty()) {
+                System.out.println("Fine, be that way!");
+            } else if (message.equalsIgnoreCase("Bye") || message.equalsIgnoreCase("Bye.")) {
+                System.out.println("Bob continues to stare blankly into his cell phone.");
+                break;
+            } else {
+                System.out.println("Whatever.");
+            }
+        } while (true);
     }
 }

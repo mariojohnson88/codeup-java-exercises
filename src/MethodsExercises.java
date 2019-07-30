36cc712d94abce44f867be82c524b9797c26c093
Mario Johnson <johnson.mario8404@gmail.com>
2019-07-30 09:07:00 -0500

    Updated Methods Exercise

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
-
-
+import java.util.*;
 //    visibility ownership ReturnType medthodName(ParamType paramName,...) {
 //    return something if the ReturnType is something other than void
 //        }
 
+
+//1.
 public class MethodsExercises {
     public static double addition(double a, double b){
         return a + b;
@@ -12,8 +13,15 @@ public class MethodsExercises {
     public static double subtraction(double c, double d){
         return c - d;
     }
+
+//    BONUS a multiplication method using a loop with recursion w/o *
     public static double multiplication (double e, double f){
-        return e * f;
+//        return e * f;
+       double result = 0;
+       for(int i = 0; i < f; i++){
+           result += e;
+       }
+       return result;
     }
     public static double division (double g, double h){
         return g / h;
@@ -21,6 +29,15 @@ public class MethodsExercises {
     public static double modulus (double i, double j){
         return i % j;
     }
+    static void randomIntegers() {
+        double randomDouble = Math.random();
+        randomDouble = randomDouble * 12 + 1;
+        int randomInt = (int) randomDouble;
+        System.out.println(randomInt);
+    }
+
+
+//2.
     public static int getInteger(double min, double max) {
 
         Scanner scanner = new Scanner(System.in);
@@ -45,17 +62,47 @@ public class MethodsExercises {
         System.out.println(division(20,5));
         System.out.println(modulus(100,25));
 
-        long theFactorial = getInteger(1,10);
+
+        long theFactorial = getInteger(1,15);
         long helpfulInt = theFactorial;
         for(long i = 1; i < helpfulInt; i++){
             theFactorial = theFactorial * i;
+
+
         }
-        Scanner sc2 = new Scanner(System.in);
-        System.out.print("Would you like to continue [y/N] ");
-        String userInput2 = sc2.next();
-        boolean newConfirmation = userInput2.equals("y");
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Would you like to continue and see the factorial of this number? [y/N] ");
+        String willTheyContinue = scanner2.next();
+        boolean newConfirmation = willTheyContinue.equalsIgnoreCase("y");
         if(newConfirmation){
             System.out.println("The Factorial of " + helpfulInt + " is " + theFactorial);
+
+            Scanner scanner3 = new Scanner(System.in);
+            System.out.println("How about trying your luck, wanna roll the dice?: [y/N] ");
+            String diceQuestion = scanner3.next();
+            boolean diceConformation = diceQuestion.equalsIgnoreCase("y");
+            if (diceConformation) {
+                System.out.println("First off. How many sides are on a pair if dice? ");
+
+                do {
+                    int howManySides = scanner2.nextInt();
+                    int sides = 12;
+                    if (howManySides < sides) {
+                        System.out.println("Wow that's way too low ");
+                    } else if (howManySides > sides) {
+                        System.out.println("Okay, come on now. WAY too many!");
+                    } else if (howManySides == sides) {
+                        System.out.println("Nice! You might out as well roll the dice. \"DO IIITTT\" -Shia Labouf: [y/N]");
+                        String rollDice = scanner3.next();
+                        boolean newestConfirmation = rollDice.equalsIgnoreCase("y");
+                        if(newestConfirmation){
+                            System.out.print("You rolled a: ");
+//                            PUt random number generator here
+                                randomIntegers();
+                        }
+                    }
+                } while (true);
+            }
         }
     }
 }

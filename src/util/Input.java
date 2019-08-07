9c0a50b61bae8a0e782f1a5fe22019c07e1665b4
Mario Johnson <johnson.mario8404@gmail.com>
2019-08-07 10:13:43 -0500

    Refactored Completed Code

--- src/util/Input.java
+++ src/util/Input.java
@@ -7,15 +7,19 @@ public class Input {
     public Input (){
         this.scanner = new Scanner(System.in);
     }
+    public String getString(){
+        System.out.println("Type a string, pick anything: ");
+        return this.scanner.nextLine();
+    }
 
     public String getString(String prompt){
-//        System.out.println("Type a string, pick anything: ");
+        System.out.println("Type a string, pick anything: ");
         System.out.println(prompt);
         return this.scanner.nextLine();
     }
 
     public boolean yesNo(){
-//        System.out.println("Please decide yes or no: [y/N] ");
+        System.out.println("Please decide yes or no: [y/N] ");
         String answer = scanner.next();
         boolean newConfirmation = answer.contains("y");
         if (newConfirmation) {
@@ -44,7 +48,7 @@ public class Input {
         } while(true);
     }
 
-    public int getInt(String prompt  ){
+    public int getInt(String prompt) {
 //        System.out.println("Give me any integer: ");
         System.out.println(prompt);
         return this.scanner.nextInt();
@@ -67,15 +71,15 @@ public class Input {
 
     public double getDouble(String prompt) {
         System.out.println(prompt);
-        return this.getDouble();
-//        Scanner scanner = new Scanner(System.in);
+//        return this.getDouble();
+        Scanner scanner = new Scanner(System.in);
 //        System.out.print("Enter a number for the area and circumference: ");
-//        double inputDec = scanner.nextDouble();
-//        return inputDec;
+        double inputDec = scanner.nextDouble();
+        return inputDec;
     }
 
     public double getDouble() {
-//        System.out.println("Choose a number that is type double: ");
+        System.out.println("Choose a number that is type double: ");
         return this.scanner.nextDouble();
     }
 }
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -3,12 +3,14 @@ package util;
 public class InputTest {
     public static void main(String[] args) {
         Input input = new Input();
-//        System.out.println(input.getString());
+        int test = input.getInt("Enter an integer");
+        System.out.println(test);
+        System.out.println(input.getString());
         System.out.println(input.yesNo());
         System.out.println(input.getInt(-200,200));
 //        System.out.println(input.getInt());
-        System.out.println(input.getDouble(-1000,1000));
-        System.out.println(input.getDouble());
+        System.out.println(input.getDouble(3985, 49852));
+        System.out.println(input.getDouble("Enter a double"));
     }
 }
 
